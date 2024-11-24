effadfa82ad71b17f2cde395f07131b25d633f12
Joshua Goon <j.j.jappiecraft@gmail.com>
2024-11-23 22:32:13 -0800

    re-add api sdk test fetch

--- client-v2/app/(home)/index.tsx
+++ client-v2/app/(home)/index.tsx
@@ -1,12 +1,30 @@
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { getGreetingByName } from "@/gen/openapi";
+import { AxiosError } from "axios";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Page() {
   const { user } = useUser();
 
+  const [name, setName] = useState<string>("");
+
+  useEffect(() => {
+    const loadName = async () => {
+      try {
+        const res = await getGreetingByName("name");
+        setName(res.data.message);
+      } catch (e) {
+        if (e instanceof AxiosError) console.log(e.message);
+      }
+    };
+
+    loadName();
+  }, []);
+
   return (
-    <View>
+    <View className="justify-center items-center flex-1">
+      <Text>Name: {name || "no name"}</Text>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
       </SignedIn>
